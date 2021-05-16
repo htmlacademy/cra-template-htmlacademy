4eba0a7f3736b7aed6074ad0a53a8c30789dcf84
kam4atka <sledsib@ya.ru>
2021-05-17 01:39:10 +0300

    Внесет правки для app.test.jsx

--- template/src/components/app/app.test.js
+++ template/src/components/app/app.test.js
@@ -1,7 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import {render, screen} from '@testing-library/react';
 import App from './app';
 
-test('Renders app-component', () => {
+test(`Renders app-component`, () => {
   render(<App />);
   const textElement = screen.getByText(/Hello, world!/i);
   expect(textElement).toBeInTheDocument();
